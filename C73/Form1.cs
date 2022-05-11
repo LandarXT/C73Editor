03e17b90dc5106c2e2936a4155d0fe7fcc9b657f
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-05-11 20:15:11 +0100

    Add project files.

---
+++ C73/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C73
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Font = fontDialog1.Font;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Title = "Save";
+            saveDialog.Filter = "Text Files (*.txt)|*.txt" + "|" +
+
+                                "All Files (*.*)|*.*";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filetosave = saveDialog.FileName;
+                System.IO.StreamWriter writer = new System.IO.StreamWriter(filetosave); 
+                writer.Write(richTextBox1.Text.ToString()); 
+                writer.Close(); 
+                writer.Dispose();
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); 
+            if (result == DialogResult.OK) 
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    richTextBox1.Text = text;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            richTextBox1.Font = fontDialog1.Font;
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                richTextBox1.Font = fontDialog1.Font;
+            }
+
+
+        }
+
+    }
+}
+
