429666ceacde75993fb9cab5ff053b9092bfbc90
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-05-12 19:24:44 +0100

    Fullscreen

--- C73/Form1.Designer.cs
+++ C73/Form1.Designer.cs
@@ -39,6 +39,7 @@ namespace C73
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(720, 7);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(68, 20);
@@ -49,6 +50,9 @@ namespace C73
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Location = new System.Drawing.Point(4, 33);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(781, 411);
@@ -57,6 +61,7 @@ namespace C73
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Location = new System.Drawing.Point(646, 7);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(68, 20);
